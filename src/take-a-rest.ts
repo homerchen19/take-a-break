de651d0104adb7fa929689702e661fa3c2102ec1
xxhomey19 <xxhomey19@gmail.com>
2019-01-09 23:31:47 +1100

    feat: Set basic questions and tweak tslint, roolup

--- src/take-a-rest.ts
+++ src/take-a-rest.ts
@@ -1,4 +1,27 @@
-// Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
-// import "core-js/fn/array.find"
-// ...
-export default class DummyClass {}
+import inquirer, { Questions } from 'inquirer';
+
+const questions: Questions = [
+  {
+    name: 'time',
+    message: 'How long?',
+    type: 'list',
+    choices: ['5 mins', '10 mins', '15 mins', '20 mins', '30 mins', 'other'],
+  },
+  {
+    name: 'other',
+    message: 'How many minutes?',
+    type: 'input',
+    when: answers => answers.time === 'other',
+    validate: input => {
+      if (Number(input)) {
+        return true;
+      }
+
+      return 'Please provide a number';
+    },
+  },
+];
+(async function() {
+  const answers = await inquirer.prompt(questions);
+  console.log(answers);
+})().catch(() => console.error('oops, something wrong'));
